e10303f1f77c7d7fb0a7655cf23a74c6f43c0c9c
LYD031106 <2565506388@qq.com>
2024-03-16 21:07:21 +0800

    LYD

--- Image Classification/liner_classfication.py
+++ Image Classification/liner_classfication.py
@@ -9,38 +9,57 @@ from assignment1_colab.assignment1.cs231n.data_utils import load_CIFAR10
 import numpy as np
 
 class Liner_classification:
-    def __init__(self,input_size,output_size):
-        self.W = np.random.randn(input_size,output_size)
-        self.b = np.random.randn(output_size)
+    def __init__(self,output_size,X_train,Y_train):
+        self.W = np.random.randn(X_train.shape[1],output_size)
+        self.X_train = X_train
+        self.Y_train = Y_train
 
-    def predict(self,X_test,Y_test,reg):
+    def train_svm_classification(self,reg):
         """
-        :param X_test: 包含一个所有数据的形状为 (N, D) 的 NumPy 数组，其中 N 是测试集的数量，D 是特征的维度
-        :param Y_test: 测试集中的数据
-        :param reg: 正则化的值，使用L1正则化
+        :param reg: 学习率
         :return:
         """
         total_loss = 0
-        for i in range(X_test.shape[0]):
-            output = self.forward(X_test[i])
-            loss = self.SVMLoss(output,Y_test[i])
-            total_loss += loss
+        dw = np.zeros(self.W.shape)
+        for i in range(self.X_train.shape[0]):
+            output = self.forward(self.X_train[i])
+            correct_score = output[self.Y_train[i]]
+            for j in range(self.X_train.shape[1]):
+                if j == self.Y_train[i]:
+                    continue
+                else:
+                    loss = output[j] - correct_score + 1
+                    if loss > 0:
+                       dw[:,Y_train[j]] += -self.X_train[i]
+                       dw[:,j] += self.X_train[i]
+                       total_loss += loss
+        total_loss /= self.X_train.shape[0]
+        dw /= self.X_train.shape[0]
         total_loss += reg*np.sum(self.W * self.W)
         #对于total_loss进行均值
-        total_loss /= X_test.shape[0]
-
-    def minbatchpredict(self,X_test,Y_test):
+        return total_loss,dw
+    def train_svm_classification_byvector(self,reg):
         """
-        :param X_test:  包含一个小批量数据的形状为 (N, D) 的 NumPy 数组，其中 N 是批量大小，D 是特征的维度
-        :param Y_test:  维度是（N）,存储了小批量数据的label值
-        :return: 返回loss值
+        :param reg: 学习率
+        :return:
         """
-        output = self.forward(X_test)
-        loss = self.SVMLoss(output,Y_test)
+        score = self.X_train.dot(self.W)
+        #计算loss
+        correct_score = score[np.arange(score.shape[0]),self.Y_train][:, np.newaxis]
+        score = score - correct_score + 1
+        score[range(score.shape[0]), self.Y_train] = 0
+        midloss = np.maximum(0,score)
+        loss = np.sum(score,axis=1)
+        total_loss = np.sum(loss)/self.X_train.shape[0] + reg * np.sum(self.W * self.W)
+        return score
 
+
+
+    def gradienct(self,loss):
+        pass
     def forward(self,input):
         output = np.dot(input , self.W)
-        output = output + self.b
+        output = output
         return output
 
     def SVMLoss(self,output,label):
@@ -55,6 +74,9 @@ X_train,Y_train,X_test,Y_test = load_CIFAR10("../data/cifar-10-python")
 X_train = X_train.reshape(X_train.shape[0],X_train.shape[1]*X_train.shape[2]*X_train.shape[3])
 X_test = X_test.reshape(X_test.shape[0],X_test.shape[1]*X_test.shape[2]*X_test.shape[3])
 
-model = Liner_classification(X_train.shape[1],output_size = 10)
-model.predict(X_test,Y_test,1e-9)
+mean_image = np.mean(X_train,axis = 1)
+
+
+model = Liner_classification(output_size = 10,X_train = X_train , Y_train = Y_train)
+model.train_svm_classification_byvector(reg = 1e-9)
 
